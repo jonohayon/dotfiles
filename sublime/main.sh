5b5120f8af60421347c4649bf1c14302eede35be
Jonathan Ohayon <me@jonathano.com>
2017-04-22 15:27:25 +0300

    Added Sublime Text 3 executable linking

--- sublime/main.sh
+++ sublime/main.sh
@@ -4,5 +4,9 @@ sublime_routine () {
     cp "./sublime/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
     cp "./sublime/Package Control.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings"
     ok "Configured Sublime Text 3!"
+
+    message "Linking Sublime Text 3 executable..."
+    ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" "$HOME/bin/subl"
+    ok "Linked Sublime Text 3 executable!"
   title_off
 }
